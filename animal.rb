6bd938114bfd1be66470a63191abb1e424098349
w.z <justin.seeley.cn@gmail.com>
2016-08-23 17:24:24 +0800

    CAPGMath

---
+++ animal.rb
@@ -0,0 +1,35 @@
+# Documentation: https://github.com/Homebrew/brew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Animal < Formula
+  desc "Another Math Library"
+  homepage "https://github.com/autosquid/ANIMAL"
+  url "https://github.com/autosquid/ANIMAL/archive/1.0.2.zip"
+  version "1.0.2"
+  sha256 "39387ddc2f4180bb4b6bfb2b9a9ad40bc90f209fdaf232cc2f3a679be48a613c"
+
+  depends_on "cmake" => :build
+  # depends_on :x11 # if your formula requires any X11/XQuartz components
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test ANIMAL`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "true"
+  end
+end
